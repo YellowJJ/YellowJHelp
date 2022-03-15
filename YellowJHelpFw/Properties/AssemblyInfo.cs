3c0fb1a1948f65740fe7357933bb51e63270289f
YellowJ <943324305@qq.com>
2022-03-15 16:56:33 +0800

    发布nuget版本：1.1.3

--- YellowJHelpFw/Properties/AssemblyInfo.cs
+++ YellowJHelpFw/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle("YellowJHelpFw")]
-[assembly: AssemblyDescription("YellowJHelp Framework 4.6.1++++/（调整kafka返回Offset）")]
+[assembly: AssemblyDescription("YellowJHelp Framework 4.6.1++++/（Redis更换NewLife-日均百亿次项目验证）")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("YellowJHelpFw")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 //可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值
 //通过使用 "*"，如下所示:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.2.0")]
-[assembly: AssemblyFileVersion("1.1.2.0")]
+[assembly: AssemblyVersion("1.1.3.0")]
+[assembly: AssemblyFileVersion("1.1.3.0")]
